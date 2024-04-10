a109cd1541be702bbf174837c0ae3ed596b2689e
IAmProgrammist <vladislav.pakhomov@bk.ru>
2024-04-10 07:36:24 +0300

    Lab 9 completed

---
+++ src/libs/alg/mathparser/mathparser.cpp
@@ -0,0 +1,3 @@
+#include "mathparser.h"
+
+MathParser::MathParser() {}
---
+++ src/libs/alg/mathparser/mathparser.h
@@ -0,0 +1,10 @@
+#ifndef MATHPARSER_H
+#define MATHPARSER_H
+
+class MathParser
+{
+public:
+    MathParser();
+};
+
+#endif // MATHPARSER_H

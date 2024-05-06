b118c9a7e5bd18116fc3a1dec826d6e81c3aa735
IAmProgrammist <vladislav.pakhomov@bk.ru>
2024-05-07 01:10:11 +0300

    Lab 12 completed

--- src/lab12/main.py
+++ src/lab12/main.py
@@ -3,10 +3,12 @@ def main():
     columns = int(input("Введите количество столбцов: "))
     islands = list()
     discovered = list()
+    route = list()
 
     for _ in range(0, lines):
         islands.append(list(bool(int(x)) for x in input().split()))
         discovered.append([-1] * columns)
+        route.append([(-1, -1)] * columns)
 
     x_beg, y_beg = input("Введите координаты точки начала: ").split()
     x_end, y_end = input("Введите координаты точки конца: ").split()
@@ -16,6 +18,8 @@ def main():
     process_queue = [(x_beg, y_beg)]
     counter = 0
     discovered[x_beg][y_beg] = counter
+    route[x_beg][y_beg] = (x_beg, y_beg)
+
     while len(process_queue) > 0 and discovered[x_end][y_end] == -1:
         counter += 1
         process_queue_tmp = list()
@@ -31,14 +35,22 @@ def main():
                                                                                            islands[x[0]][x[1]] and
                                                                                            discovered[x[0]][x[1]] == -1]:
                 discovered[valid_pos[0]][valid_pos[1]] = counter
+                route[valid_pos[0]][valid_pos[1]] = current_element
                 process_queue_tmp.append(valid_pos)
         process_queue = process_queue_tmp
 
     if discovered[x_end][y_end] == -1:
         print("Точки достигнуть невозможно")
     else:
-        print(f"Путь до точки: {discovered[x_end][y_end]}")
+        print(f"Длина пути до точки: {discovered[x_end][y_end]}")
 
+        el = route[x_end][y_end]
+        print((x_end, y_end), '<-', end=" ")
+        while el != route[el[0]][el[1]]:
+            print(el, '<-', end=" ")
+            el = route[el[0]][el[1]]
+
+        print(el)
 
 if __name__ == "__main__":
     main()
